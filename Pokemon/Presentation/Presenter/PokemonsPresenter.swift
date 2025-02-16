42cd7cfcb98e0d7bf3f967a53dac3c3c7a37e2e5
Schiavo <schiavo1009@gamil.com>
2025-02-16 18:26:07 -0300

    Added PokemonPresenter and your tests

--- Pokemon/Presentation/Presenter/PokemonsPresenter.swift
+++ Pokemon/Presentation/Presenter/PokemonsPresenter.swift
@@ -1,10 +1,11 @@
 import Foundation
 protocol PokemonsPresenter {
     func viewDidLoad()
-    func fetchPokemons() async
+    func fetchPokemons(loadMore: Bool) async
     var pokemons: [PokemonEntity]? {get set}
     var isLoading: Bool {get set}
-
+    var isLoadingBottom: Bool {get set}
+    var error: String? {get set}
 }
 
 class PokemonsPresenterImpl: PokemonsPresenter, ObservableObject {
@@ -12,6 +13,8 @@ class PokemonsPresenterImpl: PokemonsPresenter, ObservableObject {
     private var fetchPokemonsUsecase: FetchPokemonsUsecase
     @Published var pokemons: [PokemonEntity]?
     @Published var isLoading: Bool = false
+    @Published var isLoadingBottom: Bool = false
+    @Published var error: String?
     
     init(fetchPokemonsUsecase: FetchPokemonsUsecase) {
         self.fetchPokemonsUsecase = fetchPokemonsUsecase
@@ -23,17 +26,39 @@ class PokemonsPresenterImpl: PokemonsPresenter, ObservableObject {
         }
     }
     
-    func fetchPokemons() async {
-        isLoading = true
-        let result = await fetchPokemonsUsecase.getPokemons(offset: 0, limit: 20)
+    func fetchPokemons(loadMore: Bool = false) async {
+        error = nil
+        
+        if(loadMore){
+            isLoadingBottom = true
+        } else{
+            isLoading = true
+        }
+        
+        let result = await fetchPokemonsUsecase.getPokemons(offset: pokemons?.count ?? 0, limit: 20)
+        
+        isLoadingBottom = false
         isLoading = false
+        
         switch result {
         case .success(let pokemons):
-            self.pokemons = pokemons
-        case .failure:
-            pokemons = nil
-            
+            handlerSuccess(pokemons: pokemons, loadMore: loadMore)
+        case .failure(let error):
+            if !loadMore {
+                handleError(error: error)
+            }
         }
     }
     
+    func handlerSuccess(pokemons: [PokemonEntity], loadMore: Bool = false) {
+        if loadMore {
+            self.pokemons?.append(contentsOf: pokemons)
+            return
+        }
+        self.pokemons = pokemons
+    }
+    
+    func handleError(error: Error) {
+        self.error = "Unexpected error, try again"
+    }
 }
--- PokemonTests/Presentation/Presenter/PokemonsPresenterTest.swift
+++ PokemonTests/Presentation/Presenter/PokemonsPresenterTest.swift
@@ -5,6 +5,7 @@ import XCTest
 class PokemonsPresenterTest: XCTestCase {
     var sut: PokemonsPresenter!
     var fetchPokemonsUsecase: FetchPokemonsUsecaseSpy!
+    let pokemon = PokemonEntity(id: 1, name: "Bulbasaur")
     
     override func setUp() {
         super.setUp()
@@ -20,24 +21,64 @@ class PokemonsPresenterTest: XCTestCase {
     
     func testVariablePokemonsShouldBeFillWhenFetchUsecaseReturnSuccess() async throws {
         XCTAssertEqual(sut.pokemons, nil)
-        await sut.fetchPokemons()
+        await sut.fetchPokemons(loadMore: false)
         XCTAssertEqual(sut.pokemons, [])
     }
     
+    func testWithOffsetAndLimitParamsInFirstCallFetchPokemon() async throws {
+        await sut.fetchPokemons(loadMore: false)
+        XCTAssertEqual(fetchPokemonsUsecase.offset, 0)
+        XCTAssertEqual(fetchPokemonsUsecase.limit, 20)
+    }
+    
     func testVariablePokemonsShouldBeFillQithPokemonsWhenFetchUsecaseReturn() async throws {
-      let pokemonsList = [PokemonEntity(id: 1, name: "Bulbasaur")]
+      let pokemonsList = [pokemon]
         fetchPokemonsUsecase.mockedResponse = pokemonsList
-      await sut.fetchPokemons()
+      await sut.fetchPokemons(loadMore: false)
         XCTAssertEqual(sut.pokemons, pokemonsList)
     }
     
     func testLoadingVariableChangeWithCalledUsecase() async throws {
         sut.isLoading = true
-        await sut.fetchPokemons()
+        await sut.fetchPokemons(loadMore: false)
         XCTAssertEqual(sut.isLoading, false)
     }
     
     func testErrorFetchPokemonsWhenErrorIsGenericError() async throws {
+        XCTAssertEqual(sut.error, nil)
+        fetchPokemonsUsecase.mockedError = GenericError()
+        await sut.fetchPokemons(loadMore: false)
+        XCTAssertEqual(sut.error, "Unexpected error, try again")
+    }
+    
+    func testCalledFetchPokemonOnceAfterErrorAndErrorShouldBeNil() async throws {
+        fetchPokemonsUsecase.mockedError = GenericError()
+        await sut.fetchPokemons(loadMore: false)
+        XCTAssertEqual(sut.error, "Unexpected error, try again")
+        fetchPokemonsUsecase.mockedError = nil
+        fetchPokemonsUsecase.mockedResponse = []
+        await sut.fetchPokemons(loadMore: false)
+        XCTAssertEqual(sut.error, nil)
+    }
+    
+    func testShouldCallFetchPokemonForPagination() async throws {
+        fetchPokemonsUsecase.mockedResponse = [
+            pokemon
+        ]
+        fetchPokemonsUsecase.mockedError = nil
+        await sut.fetchPokemons(loadMore: false)
+        XCTAssertEqual(sut.isLoading, false)
+        XCTAssertEqual(sut.pokemons, [pokemon])
+        await sut.fetchPokemons(loadMore: true)
+        XCTAssertEqual(fetchPokemonsUsecase.offset, 1)
+        XCTAssertEqual(fetchPokemonsUsecase.limit, 20)
+        XCTAssertEqual(sut.pokemons, [pokemon, pokemon])
+    }
+    
+    func testFetchpokemonsShouldNotDoAnyErrorWhenCalledWithLoadMore() async throws {
+        fetchPokemonsUsecase.mockedError = GenericError()
+        await sut.fetchPokemons(loadMore: true)
+        XCTAssertEqual(sut.error, nil)
 
     }
     
@@ -46,11 +87,16 @@ class PokemonsPresenterTest: XCTestCase {
 class FetchPokemonsUsecaseSpy : FetchPokemonsUsecase {
     var getCalledCount = 0
     var mockedResponse: [PokemonEntity] = []
-    
+    var mockedError: Error?
+    var offset: Int?
+    var limit: Int?
     func getPokemons(offset: Int?, limit: Int?) async -> Result<[PokemonEntity], any Error> {
+        self.offset = offset
+        self.limit = limit
         getCalledCount += 1
+        if let error = mockedError{
+            return .failure(error)
+        }
         return .success(mockedResponse)
     }
-    
-    
 }
